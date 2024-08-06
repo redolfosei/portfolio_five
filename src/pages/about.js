510d74a5360bd80d34080149f32838d130016bd5
RedolF <redolf639@gmail.com>
2024-08-06 23:11:59 +0000

    do I still have all files

--- src/pages/about.js
+++ src/pages/about.js
@@ -51,7 +51,7 @@ const about = () => {
                 Biography
               </h2>
               <p className="font-medium">
-                Hi, I'm Redolf Kwame Osei, a web developer and UI/UX designer
+                Hi, I&apos'm Redolf Kwame Osei, a web developer and UI/UX designer
                 with a passion for creating beautiful, functional, and
                 user-centered digital experiences. With 4 years of experience in
                 the field. I am always looking for new and innovative ways to
@@ -59,11 +59,11 @@ const about = () => {
               </p>
               <p className="my-4 font-medium">
                 I believe that design is about more than just making things look
-                pretty – it's about solving problems and creating intuitive,
+                pretty – it&apos's about solving problems and creating intuitive,
                 enjoyable experiences for users.
               </p>
               <p className="font-medium">
-                Whether I'm working on a website, mobile app, or other digital
+                Whether I&apos'm working on a website, mobile app, or other digital
                 product, I bring my commitment to design excellence and
                 user-centered thinking to every project I work on. I look
                 forward to the opportunity to bring my skills and passion to
