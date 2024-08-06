a255cf0b6548e1f88440260199b56b6eebd44083
RedolF <redolf639@gmail.com>
2024-08-06 22:59:07 +0000

    do I still have all files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 
-
-const { fontFamily } = require('tailwindcss/defaultTheme')
+const { fontFamily } = require("tailwindcss/defaultTheme")
 
 module.exports = {
   content: [
@@ -11,22 +10,22 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        mont: ['var(--font-mont)', ...fontFamily.sans],
+        mont: ["var(--font-mont)", ...fontFamily.sans],
       },
-      colors:{
+      colors: {
         dark: "#1b1b1b",
         light: "#f5f5f5",
         primary: "#B63E96", // 240,86,199
         primaryDark: "#58E6D9", // 80,230,217
       },
       animation: {
-        'spin-slow': 'spin 9s linear infinite',
+        "spin-slow": "spin 9s linear infinite",
       },
-      backgroundImage:{
-        circularLight: 'repeating-radial-gradient(rgba(0,0,0,0.4) 2px,#f5f5f5 5px,#f5f5f5 100px);'
-      }
+      backgroundImage: {
+        circularLight:
+          "repeating-radial-gradient(rgba(0,0,0,0.3) 2px,#f5f5f5 3px,#f5f5f5 10%);",
+      },
     },
   },
   plugins: [],
 }
-
